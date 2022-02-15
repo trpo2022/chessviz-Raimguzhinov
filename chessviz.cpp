b4afbef953f23d1d61be71911372feed00520107
Raimguzhinov <d.raimguzhinov@stud.sibguti.ru>
2022-02-15 16:08:21 +0700

    Add board output in chessviz.cpp

--- chessviz.cpp
+++ chessviz.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <fstream>
 #include <unistd.h>
+#include <ostream>
+#define board_size 8
 
 using namespace std;
 
@@ -18,6 +20,24 @@ void loading(void) {
         usleep(100000);
         cout << "\b-" << flush;
     }
+}
+
+void board(void) {
+    char arr[board_size][board_size] = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+               {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+               {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+               {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+               {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+               {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+               {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+               {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
+    for(int i = 0; i < board_size; i++) {
+        cout << board_size - i; 
+        for(int j = 0; j < board_size; j++)
+            cout << " " << arr[i][j];
+        cout << endl;
+    }
+    cout << "  a b c d e f g h\n";
 }
 
 int main(int argc, char* argv[]) {
@@ -47,7 +67,7 @@ int main(int argc, char* argv[]) {
     }
     notation.close(); //закрываем чтение
 
-    system ("clear"); //очищаем консоль чтоб было больше места    
-    cout << "здесь будет доска" << endl;
+    system ("clear"); //очищаем консоль чтоб было больше места
+    board(); 
     return 0;
 }
