e3742014ee325e2334b17b744f082f71d83ca2d9
Raimguzhinov <d.raimguzhinov@stud.sibguti.ru>
2022-05-12 17:15:44 +0700

    Change the parsing function

--- src/libchessviz/open_game.cpp
+++ src/libchessviz/open_game.cpp
@@ -11,11 +11,27 @@ int readCommand(
         int move_cnt,
         char boardrr[board_size][board_size])
 {
+    int flag;
     size_t format = line.find_first_of("KQRBN");
+    size_t format2
+            = line.find_first_of("qwrtyuiop{}[]sjkl;'zvnm,./?~!@#$%^&*()_=");
     while (format != string::npos) {
         line.erase(line.begin() + format);
         format = line.find_first_of("KQRBN", format + 1);
     }
+    while (format2 != string::npos) {
+        flag = 25;
+        if (checkStep(line, motion, boardrr, true, flag) == false)
+            break;
+        line.erase(line.begin() + format2);
+        format2 = line.find_first_of(
+                "qwrtyuiop{}[]sjkl;'zvnm,./?~!@#$%^&*()_=", format2);
+        break;
+    }
+    if (line.find("x"))
+        flag = 13;
+    if (line.find("x"))
+        flag = 10;
     int t = line.find_first_of("-x");
     int s = line.length();
     int pos_gor1, pos_ver1, pos_gor2, pos_ver2;
@@ -37,7 +53,7 @@ int readCommand(
             move_cnt,
             boardrr);
     convertFigure(line, motion);
-    if (checkStep(line, motion, boardrr, output) == true) {
+    if (checkStep(line, motion, boardrr, output, flag) == true) {
         turnFigure(pos_gor1, pos_ver1, pos_gor2, pos_ver2, boardrr);
         return 0;
     } else
@@ -58,8 +74,8 @@ int openNotation(
     } else {
         string mv;
         int cnt = 0;
-        for (notation >> mv; !notation.eof(); notation >> mv) {
-            if (readCommand(mv, motion, cnt, boardrr) != 0)
+        for (notation >> mv; !notation.eof() + 1; notation >> mv) {
+            if (readCommand(mv, motion, cnt++, boardrr) != 0)
                 break;
         }
         cout << endl;
